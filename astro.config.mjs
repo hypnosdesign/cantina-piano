2321389611d8223ad19375c996a5a40db26706db
hypnosdesign <hypnosdesign@hypnosdesign.net>
2024-06-27 21:58:18 +0200

    netifly adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,19 @@ import { defineConfig } from 'astro/config';
 //https://www.npmjs.com/package/astro-purgecss
 import purgecss from "astro-purgecss";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    purgecss({
-      variables: true,
-      keyframes: false, 
-      safelist: {
-        greedy: [/*astro*/]
-      },
-      content: [
-        process.cwd() + '/src/**/*.{astro}' // Watching astro and vue sources (for SSR, read the note below)
-      ],
-
-    })
-  ],
+  integrations: [purgecss({
+    variables: true,
+    keyframes: false,
+    safelist: {
+      greedy: [/*astro*/]
+    },
+    content: [process.cwd() + '/src/**/*.{astro}' // Watching astro and vue sources (for SSR, read the note below)
+    ]
+  })],
   output: "server",
+  adapter: netlify()
 });
